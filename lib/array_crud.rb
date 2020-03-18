de116f197ae7e8a14d202ef3878efdcb2658d436
Kyle Bradley <kylepersonalmail@gmail.com>
2020-03-18 13:00:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  fun = ["movies", "running", "reading", "cooking"]
   
+  fun.push("fishing")
 end
 
 def add_element_to_start_of_array(array, element)
